263f89ff03d3ccf434192ff28db79b3eee4105e9
kalam409 <kalamislam409@gmail.com>
2023-01-20 23:39:29 +0600

    herosection added

--- src/components/trydoRainbowit/Pages/Home/HeroSection/HeroOne/HeroOne.jsx
+++ src/components/trydoRainbowit/Pages/Home/HeroSection/HeroOne/HeroOne.jsx
@@ -30,7 +30,7 @@ const HeroOne = () => {
               projects.map((project) => 
               <div key={project.id}>
                 <img src={project.image} alt="" />
-                <h3>{project.title}</h3>
+                <h3 className='mt-4'>{project.title}</h3>
                 <p>{project.desc}</p>
               </div>
               )
